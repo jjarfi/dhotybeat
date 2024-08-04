316b9fc9417f60c5bc06a5a12e226bec205e98a0
Pace Linux <pacelinux@gmail.com>
2024-08-04 23:25:20 +0900

    commit pertama

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        Dothy Beat by
+        <a href="https://jjarfi.github.io" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">TRITON SOFTWARE</a>
+    </div>
+</template>
